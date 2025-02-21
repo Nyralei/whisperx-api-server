864d022ecb5f482cb0ac930710891ca206fb5d84
Nyralei <93216976+Nyralei@users.noreply.github.com>
2025-02-21 22:27:41 +0300

    modify logline

--- src/whisperx_api_server/transcriber.py
+++ src/whisperx_api_server/transcriber.py
@@ -93,8 +93,7 @@ async def transcribe(
 
             diarize_model = await load_diarize_model_cached(model_name="tensorlake/speaker-diarization-3.1")
 
-            logger.info(f"Request ID: {request_id} - Diarization model loaded. Starting diarization")
-            logger.info(f"Request ID: {request_id} - Loading diarization model took {time.time() - diarization_model_start:.2f} seconds")
+            logger.info(f"Request ID: {request_id} - Diarization model loaded. Loading took {time.time() - diarization_model_start:.2f} seconds. Starting diarization")
 
             diarize_start = time.time()
 
